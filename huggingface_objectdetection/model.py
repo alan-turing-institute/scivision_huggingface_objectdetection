47841b69f2dde6fe5435f88f084d887db7533092
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-08 15:14:50 +0100

    refactor to use pipeline correctly

--- huggingface_objectdetection/model.py
+++ huggingface_objectdetection/model.py
@@ -14,12 +14,10 @@ from transformers import (DetrFeatureExtractor,
 def tidy_predict(self, image: np.ndarray) -> list:
     """Returns bounding boxes that can be superimposed onto an image"""
     pillow_image = Image.fromarray(image.to_numpy(), 'RGB')
-    feature_extractor = self.feature_extractor(images=pillow_image, return_tensors="pt")
-    model = self.pretrained_model(**feature_extractor)
     object_detector =  pipeline("object-detection",
-                                model = model,
-                                feature_extractor = feature_extractor
-                               )
+                                model = self.pretrained_model,
+                                feature_extractor = self.feature_extractor
+                                )
     return object_detector(pillow_image)
     
     
